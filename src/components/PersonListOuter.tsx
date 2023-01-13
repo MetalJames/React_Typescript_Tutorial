1aa04402e3991bb05e5e0682ee172bceec4c8772
Volodymyr <volodyaruzhak@gmail.com>
2023-01-13 17:30:56 -0500

    creating basic types and using them in multiple places

---
+++ src/components/PersonListOuter.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Name } from './Person.types';
+
+type PersonListProps = {
+    names: Name[]
+}
+
+const PersonList = (props: PersonListProps) => {
+    return (
+        <>
+            {props.names.map(name => {
+                return (
+                    <h2 key={name.first}>{name.first} {name.last}</h2>
+                )
+            })}
+        </>
+    )
+}
+
+export default PersonList
